c2b4a616dc1306ec92634512028e094369c3c161
Andy Haynes <andyghaynes@gmail.com>
2019-11-13 21:49:16 -0800

    ignore image import errors

--- app/images/index.ts
+++ app/images/index.ts
@@ -1,6 +1,10 @@
+// @ts-ignore
 import grain from './grain.png';
+// @ts-ignore
 import hop from './hop.png';
+// @ts-ignore
 import mash from './mash.png';
+// @ts-ignore
 import yeast from './yeast.png';
 
 export const icons = {
