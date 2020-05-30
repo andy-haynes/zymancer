f4217e2d739a3deeb14ab902d743cf285a519821
Andy Haynes <andyghaynes@gmail.com>
2020-05-30 14:02:09 -0700

    use navigation hook over service

--- app/components/navigation/image_row.tsx
+++ app/components/navigation/image_row.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Image, Text, TouchableOpacity, View } from 'react-native';
 
-import NavigationService from '../../services/navigation';
+import { useNavigator } from '../../hooks/navigation';
 import { ImageNavRoute } from '../../types/routes';
 import styles from './styles/image_row.style';
 
@@ -10,13 +10,14 @@ type Props = {
 };
 
 export default function ImageRow({ navOption }: Props) {
+  const { navigateToScreen } = useNavigator();
   return (
     <TouchableOpacity
       activeOpacity={0.6}
       delayPressIn={80}
       key={navOption.label}
       style={styles.row}
-      onPress={() => navOption.screen && NavigationService.navigate(navOption.screen)}
+      onPress={() => navigateToScreen(navOption.screen)}
     >
       <View style={styles.imageBox}>
         <Image
