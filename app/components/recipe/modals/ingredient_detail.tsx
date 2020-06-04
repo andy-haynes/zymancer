84bbd9644d08dd668eb118aeb18327fd0606781c
Andy Haynes <andyghaynes@gmail.com>
2020-06-03 21:19:29 -0700

    ingredient detail modal

---
+++ app/components/recipe/modals/ingredient_detail.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import { IngredientType } from '../../../constants/recipe';
+import { icons } from '../../../images';
+import { Image, Modal } from '../../core';
+
+type Props = {
+  children: React.ReactNode;
+  ingredientType: IngredientType;
+  isVisible: boolean;
+  name: string;
+  onDismissModal: () => void;
+};
+
+function getHeaderImage(ingredientType: IngredientType): number {
+  return {
+    [IngredientType.Hop]: icons.hop,
+    [IngredientType.Malt]: icons.grain,
+    [IngredientType.Yeast]: icons.yeast,
+  }[ingredientType];
+}
+
+export default function IngredientDetailModal({
+  children,
+  ingredientType,
+  isVisible,
+  name,
+  onDismissModal,
+}: Props) {
+  return (
+    <Modal
+      headerImage={(
+        <Image
+          source={getHeaderImage(ingredientType)}
+          style={{ height: 24, width: 24 }}
+        />
+      )}
+      isVisible={isVisible}
+      onDismissModal={onDismissModal}
+      title={name}
+    >
+      {children}
+    </Modal>
+  );
+}
