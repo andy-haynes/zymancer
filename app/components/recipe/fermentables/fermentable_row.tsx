d9121234ab950b7ea902372cb7e937973613084e
Andy Haynes <andyghaynes@gmail.com>
2020-08-20 23:44:45 -0700

    clean up fermentable row styles

--- app/components/recipe/fermentables/fermentable_row.tsx
+++ app/components/recipe/fermentables/fermentable_row.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 import { Fermentable } from '../../../types/ingredients';
-import { TouchableRow } from '../../core';
+import { Column, Row, TouchableRow } from '../../core';
 import styles from './styles/fermentable_row.style';
 
 type Props = {
@@ -12,11 +12,36 @@ type Props = {
 
 export default function FermentableRow({ fermentable, selectFermentable }: Props) {
   return (
-    <TouchableRow rowStyle={styles.row} onPress={selectFermentable}>
-      <View style={[styles.color, { backgroundColor: fermentable.color }]} />
-      <Text style={styles.name}>
-        {fermentable.name}
-      </Text>
+    <TouchableRow rowStyle={styles.fermentableRow} onPress={selectFermentable}>
+      <Column style={styles.colorBar}>
+        <View style={[styles.color, { backgroundColor: fermentable.color }]} />
+      </Column>
+      <Column style={styles.fermentableDetails}>
+        <Row>
+          <Column style={styles.detailsContainer}>
+            <Row>
+              <Text style={styles.nameText}>
+                {fermentable.name}
+              </Text>
+            </Row>
+            <Row>
+              <Text style={styles.gravityText}>
+                {fermentable.gravity?.value}
+              </Text>
+            </Row>
+          </Column>
+          <Column style={styles.quantityValueContainer}>
+            <Text style={styles.quantityValueText}>
+              {fermentable.weight.value}
+            </Text>
+          </Column>
+          <Column style={styles.quantityUnitContainer}>
+            <Text>
+              {fermentable.weight.unit.shortName}
+            </Text>
+          </Column>
+        </Row>
+      </Column>
     </TouchableRow>
   );
 }
--- app/components/recipe/fermentables/styles/fermentable_row.style.ts
+++ app/components/recipe/fermentables/styles/fermentable_row.style.ts
@@ -1,17 +1,47 @@
+import _ from 'lodash';
 import { StyleSheet } from 'react-native';
 
-import { font } from '../../../../theme';
+import { getWidth } from '../../../../utils/dimensions';
+import { colors, font } from '../../../../theme';
 
 export default StyleSheet.create({
+  borderedRow: {
+    borderTopColor: colors.grayDark,
+    borderTopWidth: _.round(getWidth() * 0.8),
+  },
   color: {
     height: 80,
     width: 20,
   },
-  name: {
-    fontSize: font.size.large,
+  colorBar: {
+    flex: 1,
   },
-  row: {
+  detailsContainer: {
+    flex: 7,
+  },
+  fermentableRow: {
     height: 90,
     margin: 8,
+  },
+  fermentableDetails: {
+    flex: 9,
+    paddingVertical: 4,
+  },
+  gravityText: {
+    fontSize: font.size.medium,
+  },
+  nameText: {
+    fontSize: font.size.large,
+  },
+  quantityUnitContainer: {
+    flex: 1,
+  },
+  quantityValueContainer: {
+    flex: 2,
+    paddingRight: 8,
+  },
+  quantityValueText: {
+    fontSize: font.size.medium,
+    textAlign: 'right',
   },
 });
