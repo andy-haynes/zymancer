abe00e46b84304522cedd30fc30323d7baed3dfa
Andy Haynes <andyghaynes@gmail.com>
2020-11-07 00:02:12 -0800

    modules replaced by graphql server

--- app/types/fermentation.ts
+++
@@ -1,9 +0,0 @@
-import { DateRange, Note } from './common';
-import { Recipe } from './recipe';
-
-export type Ferment = {
-  id: string;
-  recipe: Recipe;
-  dateRange: DateRange;
-  notes?: Note[];
-};
--- app/types/zymath.ts
+++ app/types/zymath.ts
@@ -1,44 +1,6 @@
-import _ from 'lodash';
-
 export type Unit = {
   unit: string;
   name: string;
   shortName: string;
   shorterName?: string;
-};
-
-export type Measurement = {
-  value: number;
-  unit: Unit;
-};
-
-export class Range {
-  constructor(
-    public low: number,
-    public high?: number
-  ) {}
-
-  get avg() {
-    if (!this.high) {
-      return this.low;
-    }
-    return (this.high + this.low) / 2;
-  }
-
-  get values() {
-    return _.filter([this.low, this.high]);
-  }
-
-  toString() {
-    if (_.isNil(this.high)) {
-      return this.low;
-    }
-    return `${this.low} - ${this.high}`;
-  }
-}
-
-export type Ratio = {
-  antecedent: Unit;
-  consequent: Unit;
-  value: number;
 };
